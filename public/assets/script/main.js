96e2bc4c02cebd3c571f44d16d59487d81180570
Prakash Muthusamy <prakash.muthusamy@cesltd.com>
2021-10-07 15:21:21 +0530

    ASS-18 updated the readme and code files

--- public/assets/script/main.js
+++ public/assets/script/main.js
@@ -13,8 +13,15 @@
  * step5: assign a for loop and iterate the arr
  * step6: check is the iterable element is present in alphabets a to z
  * step7: if yes push in to the it_str string
- * step8: if no 
- * step9: concat all strings and print 
+ * step8: if not,
+ * step8.1: split the it_str and stored variable spl
+ * step8.2: reverse spl string and stored rev_arr
+ * step8.3: join rev_arr and stored variable res
+ * step8.4: concat variables rev and res
+ * step8.5: make empty variable it_str
+ * step8.6: concat array element to rev
+ * step9: remove the last element in the rev string
+ * step10: return rev
  */
 
 
